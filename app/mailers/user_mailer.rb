86489ca3f5625fd5757b017f111fb5a60318abec
arnoldstrelkov <wikiuspeha@gmail.com>
2015-10-13 12:44:13 +0000

    send mail4

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
     
     def welcome_email(user)
     @user = user
-    @url  = 'http://example.com/login'
+    @url  = 'http://simplecloud.ru/login'
     mail(to: 'wikiuspeha@gmail.com', subject: 'Welcome to My Awesome Site')
     end
   
